ae4c844aeb16c93c021289f28144e62cb8f70171
pianonerd7 <jxh604@case.edu>
2016-02-20 02:45:26 -0500

    button now changes size and color

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject targetObject;
+    [SerializeField]
+    private string targetMessage;
+    public Color highlightColor = Color.blue;
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("on mouse enter");
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit()
+    {
+        Debug.Log("on mouse exit");
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("you pressed start");
+        transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+    }
+
+    public void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
