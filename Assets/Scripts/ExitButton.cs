b29b8a4fef3c09c6493d4f8b26217d51640c226e
pianonerd7 <jxh604@case.edu>
2016-02-20 02:48:06 -0500

    exit button now changes size and color

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -3,9 +3,35 @@ using System.Collections;
 
 public class ExitButton : MonoBehaviour {
 
+    public Color highlightColor = Color.red;
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("on mouse enter");
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit()
+    {
+        Debug.Log("on mouse exit");
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
     public void OnMouseDown()
     {
         Debug.Log("you pressed exit");
+        transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+    }
+
+    public void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
         Application.Quit();
         UnityEditor.EditorApplication.isPlaying = false;
     }
