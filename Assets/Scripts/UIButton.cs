ced083c55bf6d8c715096a5cfe803eacd233696f
pianonerd7 <jxh604@case.edu>
2016-02-20 02:12:07 -0500

    added classes for buttons

--- Assets/Scripts/UIButton.cs
+++ Assets/Scripts/UIButton.cs
@@ -7,12 +7,14 @@ public class UIButton : MonoBehaviour {
 	public Color highlightColor = Color.cyan;
 
 	public void OnMouseEnter() {
+        Debug.Log("on mouse enter");
 		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
 		if (sprite != null) {
 			sprite.color = highlightColor;
 		}
 	}
 	public void OnMouseExit() {
+        Debug.Log("on mouse exit");
 		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
 		if (sprite != null) {
 			sprite.color = Color.white;
@@ -20,9 +22,11 @@ public class UIButton : MonoBehaviour {
 	}
 
 	public void OnMouseDown() {
+        Debug.Log("on mouse down");
 		transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
 	}
 	public void OnMouseUp() {
+        Debug.Log("on mouse up");
 		transform.localScale = Vector3.one;
 		if (targetObject != null) {
 			targetObject.SendMessage(targetMessage);
