a72dacb0c770f6b6bc6f2861ce12516137e63eab
pianonerd7 <jxh604@case.edu>
2016-02-20 19:53:15 -0500

    added more buttons

---
+++ Assets/Scripts/Grid45.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid45 : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject targetObject;
+    [SerializeField]
+    private string targetMessage;
+
+    public Color highlightColor = Color.cyan;
+    public void OnMouseEnter()
+    {
+        Debug.Log("on mouse enter");
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit()
+    {
+        Debug.Log("on mouse exit");
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("on mouse down");
+        transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
+    }
+    public void OnMouseUp()
+    {
+        Debug.Log("on mouse up");
+        transform.localScale = Vector3.one;
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
